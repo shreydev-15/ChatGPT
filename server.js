82ca0166f6f5f19b754adc857d1f35a1a15e2422
Shreyash Rai <raishreyash1519@gmail.com>
2026-09-18 11:41:41 +0530

    Modified

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const initsocketserver = require('./src/sockets/socket.server')
+const httpserver  = require('http').createServer(app)
+
 
 connectDB();
+initsocketserver(httpserver)
 
-app.listen(3100, ()=>{
+httpserver.listen(3100, ()=>{
     console.log('Server is running on port 3100');
 })
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const authroutes = require('./routes/auth.routes')
+const chatroutes = require('./routes/chat.routes')
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authroutes)
+app.use('/api/chat', chatroutes)
 
 module.exports = app;
